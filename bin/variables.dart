2c6947a0401f87ff27eaf22fcd30089003773965
alanpaul2000 <alanpaul9654@gmail.com>
2022-11-21 15:14:44 +0530

    commit

---
+++ bin/variables.dart
@@ -0,0 +1,16 @@
+void main(){
+
+  String name="ANU";
+  int age = 20;
+  int phone =6545121684;
+  double mark = 8.3;
+ String email= "anu12@gmail.com";
+
+ print("-----My Resume-----");
+ print("my name is $name");
+  print('I am $age yrs old');
+  print('My phone number is $phone');
+  print("I have $mark cgpa ");
+
+
+}
